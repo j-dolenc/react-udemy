289b031c9296df2247af34349bea9dd7fd747f39
j-dolenc <jurij1.dolenc@gmail.com>
2021-08-30 14:16:03 +0200

    končan projekt 04, minus gumbek pri cartitems ne dela..

--- 04-starting-project/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ 04-starting-project/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,29 @@
-import {useContext} from 'react';
+import {useContext, useEffect,useState} from 'react';
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) =>{
+  const [btnIsAct, setBtnIsAct] = useState(false);
   const cartCtx=useContext(CartContext);
   const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+  const {items} = cartCtx;
+  const btnClasses= `${classes.button} ${btnIsAct ? classes.bump : ''}`
+  useEffect(() => {
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setBtnIsAct(true);
+    const timer = setTimeout(() => {setBtnIsAct(false)},300);
+    return () => {
+      clearTimeout(timer);
+    }
+  },[items])
     return (
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon/>
         </span>
