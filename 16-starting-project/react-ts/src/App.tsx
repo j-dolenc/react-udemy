639a849b8ad87b890dcb6d5f0c2f85c609df5d8b
j-dolenc <jurij1.dolenc@gmail.com>
2021-09-06 10:43:28 +0200

    Pred vajo

--- 16-starting-project/react-ts/src/App.tsx
+++ 16-starting-project/react-ts/src/App.tsx
@@ -1,9 +1,11 @@
-
-import './App.css';
-
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 function App() {
+  const items = [new Todo("OKeeeej"), new Todo("Hmmm")];
+
   return (
     <div className="App">
+      <Todos items={items} />
     </div>
   );
 }
